81b8096d3143f51bf40474d2b2aef37a2b02825d
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2020-07-28 02:15:16 +0530

    Depth Card

--- src/TinderCards.jsx
+++ src/TinderCards.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import TinderCard from "react-tinder-card";
+import "./TinderCards.css";
 const TinderCards = () => {
   const [people, setPeople] = useState([
     {
       name: "Steve Jobs",
       url:
-        "https://cdn.vox-cdn.com/thumbor/TbscUHQDRx0eeh4QZljIwkf2qaY=/0x86:706x557/1400x1050/filters:focal(0x86:706x557):format(png)/cdn.vox-cdn.com/imported_assets/847184/stevejobs.png",
+        "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5b8576db31358e0429c734e3%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D211%26cropX2%3D2381%26cropY1%3D900%26cropY2%3D3072",
     },
     {
       name: "Mark Zuckerberg",
@@ -16,16 +17,22 @@ const TinderCards = () => {
   return (
     <div>
       <h1>Tinder Cards</h1>
-      {people.map((person) => (
-        <TinderCard>
-          <div
-            style={{ backgroundImage: `url(${person.url})` }}
-            className="card"
+      <div className="tinderCards__cardContainer">
+        {people.map((person) => (
+          <TinderCard
+            className="swipe"
+            key={person.name}
+            preventSwipe={["up", "down"]}
           >
-            {person.name}
-          </div>
-        </TinderCard>
-      ))}
+            <div
+              style={{ backgroundImage: `url(${person.url})` }}
+              className="card"
+            >
+              {person.name}
+            </div>
+          </TinderCard>
+        ))}
+      </div>
     </div>
   );
 };
