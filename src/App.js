529d16c1339e5464c0d38a950b28747bb6fff1c9
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2020-07-27 23:51:17 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Header from './Header';
+
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+
+        {/* Header */}
+        <Header />
+        {/* Tinder Cards */}
+        {/* Buttons below Tinder card */}
+
+
+        {/* chat screen */}
+        {/* Individual chat screen */}
+      </div>
+    </Router>
+  );
+}
+
+export default App;
